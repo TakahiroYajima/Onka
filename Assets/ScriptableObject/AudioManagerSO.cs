9351a54133f86e840b2a6d0fa94ba314bf20b1d9
TakahiroYajima <yajimatakahiro@outlook.jp>
2021-09-08 14:57:01 +0900

    ScriptableObjectコミット

---
+++ Assets/ScriptableObject/AudioManagerSO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SoundSystem;
+
+[CreateAssetMenu]
+public class AudioManagerSO : ScriptableObject {
+	//private AudioManager _audioManager;
+	//public void SetAudioManager(AudioManager audioManager) {
+	//    this._audioManager = audioManager;
+	//}
+
+	public void PlaySE(string name)
+	{
+		if (SoundManager.Instance != null) {
+            SoundManager.Instance.PlaySe(name);
+		}
+	}
+
+    public void PlaySeWithKey(string key)
+    {
+        SoundData data = DataManager.Instance.GetMenuSE(key);
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.PlaySeWithKey(key);
+        }
+    }
+    /// <summary>
+    /// 汎用サウンド用 0:ボタン押下（OK)、1:キャンセル
+    /// </summary>
+    /// <param name="_seType"></param>
+    //public void PlaySECommon(int _seType)
+    //{
+    //	if (SoundDataManager.Instance != null)
+    //	{
+    //		CommonSE commonSeType = (CommonSE)Enum.ToObject(typeof(CommonSE), _seType);
+    //		SoundDataManager.Instance.PlayCommonSE(commonSeType);
+    //       }
+    //}
+
+    //  public void PlayVOICE(string name) {
+    //      if (SoundPlayer.Instance != null) {
+    //          SoundPlayer.Instance.play(name);
+    //}
+    //  }
+}
