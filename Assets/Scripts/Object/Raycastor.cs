3a94772a3c2ceb6ea045d413e6ca758bb3e3ab25
TakahiroYajima <yajimatakahiro@outlook.jp>
2022-02-10 21:32:20 +0900

    Ray機能追加

--- Assets/Scripts/Object/Raycastor.cs
+++ Assets/Scripts/Object/Raycastor.cs
@@ -25,13 +25,28 @@ public class Raycastor : MonoBehaviour
             if(noHitCallback != null) { noHitCallback(); }
         }
     }
+    public void ScreenToRayActionWithLayerMask(int layerMask, UnityAction<RaycastHit> hitCallback, UnityAction noHitCallback = null)
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * RayDirection, Color.red, 0.5f, false);
+
+        if (Physics.Raycast(ray, out hit, RayDirection, layerMask))
+        {
+            hitCallback(hit);
+        }
+        else
+        {
+            if (noHitCallback != null) { noHitCallback(); }
+        }
+    }
 
     public void ScreenToRayActionIgnorePlayer(UnityAction<RaycastHit> hitCallback, UnityAction noHitCallback = null)
     {
         var hits = Physics.RaycastAll(camera.transform.position + (camera.transform.forward * 1.3f), camera.transform.forward, RayDirection).ToList();
-        if(hits == null || hits == default) { noHitCallback(); return; }
+        if (hits == null || hits == default) { noHitCallback(); return; }
         if (hits.Count == 0) { noHitCallback(); return; }
-        if(hits[0].transform.tag == Tags.Player)
+        if (hits[0].transform.tag == Tags.Player)
         {
             if (hits.Count > 1)
             {
@@ -49,6 +64,7 @@ public class Raycastor : MonoBehaviour
     {
         RaycastHit hit;
         Vector3 normal = (taragetPosition - startPosition).normalized;
+        Debug.DrawRay(startPosition, (taragetPosition - startPosition), Color.red, 1f);
         if (Physics.Raycast(startPosition, normal, out hit, rayDirection))
         {
             hitCallback(hit);
@@ -76,8 +92,11 @@ public class Raycastor : MonoBehaviour
     {
         RaycastHit hit;
         Vector3 normal = (taragetPosition - startPosition).normalized;
+        //Debug.DrawRay(startPosition, (taragetPosition - startPosition), Color.red, 1f);
+        
         if (Physics.Raycast(startPosition, normal, out hit, rayDirection))
         {
+            //Debug.Log(hit.transform.gameObject);
             if (Utility.Instance.IsTagNameMatch(hit.transform.gameObject, targetTag))
             {
                 return true;
@@ -85,6 +104,32 @@ public class Raycastor : MonoBehaviour
         }
         return false;
     }
+    /// <summary>
+    /// Rayが当たった対象のタグが指定したタグと一致しているか（レイヤーマスク付き）
+    /// </summary>
+    /// <param name="startPosition"></param>
+    /// <param name="taragetPosition"></param>
+    /// <param name="targetTag"></param>
+    /// <param name="layerMask"></param>
+    /// <param name="rayDirection"></param>
+    /// <returns></returns>
+    public bool IsRaycastHitObjectMatchWithLayerMask(Vector3 startPosition, Vector3 taragetPosition, string targetTag, int layerMask, float rayDirection = RayDirection)
+    {
+        RaycastHit hit;
+        Vector3 normal = (taragetPosition - startPosition).normalized;
+        //Debug.DrawRay(startPosition, (taragetPosition - startPosition), Color.red, 1f);
+
+        if (Physics.Raycast(startPosition, normal, out hit, rayDirection, layerMask))
+        {
+            //Debug.Log(hit.transform.gameObject);
+            if (Utility.Instance.IsTagNameMatch(hit.transform.gameObject, targetTag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /// <summary>
     /// カメラからRayを飛ばして当たった対象のタグが指定したタグと一致しているか
     /// </summary>
