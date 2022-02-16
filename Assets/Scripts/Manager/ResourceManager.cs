2d75f886633676a7b6774e6f30872c68a7f39c7e
TakahiroYajima <yajimatakahiro@outlook.jp>
2022-02-16 22:40:33 +0900

    リソース読み込みを共通化

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager
+{
+    public const string ItemResourcePath = "Sprites/Items/";
+    public const string CharactorResourcePath = "Sprites/Charactors/";
+
+    public static Sprite LoadResourceSprite(string resourcePath, string spriteName)
+    {
+        Texture2D tex = Resources.Load(resourcePath + spriteName) as Texture2D;
+        Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+        return sprite;
+    }
+}
